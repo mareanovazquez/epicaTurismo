f991f1154e1ca6e4baf153d9ced47f6dc4968328
Mariano Vazquez <78172034+mareanovazquez@users.noreply.github.com>
2023-10-09 15:46:45 -0300

    configuración formspree para formulario de contacto

---
+++ js/eraseForm.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contactoForm').addEventListener('submit', function() {
+        setTimeout(function() {
+            document.getElementById('nombre').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('mensaje').value = '';
+        }, 1000);
+    });
+});
