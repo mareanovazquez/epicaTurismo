31cf8d626b8e870a487b2f53de8d845367abf687
Mariano Vazquez <78172034+mareanovazquez@users.noreply.github.com>
2023-10-09 16:31:08 -0300

    ajustes en formspree y confirmación de formulario

--- js/eraseForm.js
+++ js/eraseForm.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+/* document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('contactoForm').addEventListener('submit', function() {
         setTimeout(function() {
             document.getElementById('nombre').value = '';
@@ -6,4 +6,41 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('mensaje').value = '';
         }, 100);
     });
+}); */
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    function enviarFormulario(event) {
+        event.preventDefault(); // Evita el envío regular del formulario
+        
+        // Aquí puedes añadir alguna validación adicional si lo deseas
+
+        // Envía el formulario manualmente usando Fetch API
+        fetch('https://formspree.io/f/mjvqjbpw', {
+            method: 'POST',
+            body: new FormData(document.getElementById('contactoForm')),
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.ok) {
+                // Si el envío fue exitoso, limpia el formulario
+                document.getElementById('nombre').value = '';
+                document.getElementById('email').value = '';
+                document.getElementById('mensaje').value = '';
+                document.getElementById('mensajeConfirmacion').textContent = '¡Gracias! Tu mensaje ha sido enviado con éxito.';
+            } else {
+                // Aquí puedes manejar errores, como mostrar un mensaje al usuario
+                console.error('Error enviando el formulario:', data.error);
+            }
+        })
+        .catch(error => {
+            console.error('Hubo un error al enviar el formulario:', error);
+        });
+    }
+
+    // Vinculamos la función al formulario
+    document.getElementById('contactoForm').addEventListener('submit', enviarFormulario);
 });
